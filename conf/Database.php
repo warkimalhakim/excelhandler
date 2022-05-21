5f6fc85d7d746216037762bc4e0bf52042b002a3
warkimalhakim <warkimalhakim@gmail.com>
2022-05-21 13:01:15 +0700

    clear db class

--- conf/Database.php
+++ conf/Database.php
@@ -2,24 +2,6 @@
 
 namespace Warkim\ExcelHandler;
 
-// class Connect
-// {
-//     public
-//         $host   = '',
-//         $user   = '',
-//         $pass   = '',
-//         $db     = '';
-
-//     function __construct()
-//     {
-//         try {
-//             $conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
-//         } catch (Exception $e) {
-//             return 'Database tidak terkoneksi' . $e;
-//         }
-//     }
-// }
-
 // Database connect
 $host   = 'localhost';
 $user   = 'root';
